83b002e64f0a9d09c4fe97e0e9da7f9f7af2655b
Fandus <fandus@avintec.ru>
2021-09-18 09:29:31 +0300

    v up

--- node_wrangler_octane.py
+++ node_wrangler_octane.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "Node Wrangler (Custom build for Octane)",
     "author": "Bartek Skorupa, Greg Zaal, Sebastian Koenig, Christian Brinkmann, Florian Meyer, AiSatan, Ed O'Connell",
-    "version": (0, 8),
+    "version": (0, 9),
     "blender": (2, 82, 0),
     "location": "Node Editor Toolbar or Shift-W",
     "description": "Various tools to enhance and speed up node-based workflow",
