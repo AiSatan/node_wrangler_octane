3094140b0801f02679de2128a04ad19cfa3ba1bf
Fandus <fandus@avintec.ru>
2022-10-03 19:00:00 +0300

    ver imp changes

--- node_wrangler_octane.py
+++ node_wrangler_octane.py
@@ -1,25 +1,9 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 bl_info = {
     "name": "Node Wrangler (Custom build for Octane)",
     "author": "Bartek Skorupa, Greg Zaal, Sebastian Koenig, Christian Brinkmann, Florian Meyer, AiSatan, Ed O'Connell",
-    "version": (1, 1, 6),
+    "version": (1, 2, 0),
     "blender": (2, 93, 0),
     "location": "Node Editor Toolbar or Shift-W",
     "description": "Various tools to enhance and speed up node-based workflow with Octane",
@@ -28,10 +12,6 @@ bl_info = {
     "category": "Node",
 }
 
-import os
-if os.name == 'nt':
-    from asyncio.windows_events import NULL
-    
 import bpy, blf, bgl
 import gpu
 from bpy.types import Operator, Panel, Menu
